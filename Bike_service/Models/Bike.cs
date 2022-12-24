a4f1735119f516ddf79b52fd8995f18e95aecb94
JustChris <krzysiek.palonek@gmail.com>
2022-12-24 22:12:26 +0100

    Add project files.

---
+++ Bike_service/Models/Bike.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bike_service.Models;
+
+
+public class Bike
+
+{
+    //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required(ErrorMessage ="Enter valid name!")]
+    [StringLength(50)]
+    public string Name { get; set; }
+    public int FrameLength { get; set; }
+    public string TyreType { get; set; }
+    public int WheelSize { get; set; }
+
+    public string BikeType { get; set;}
+
+    [DataType(DataType.Date)]
+    public DateTime RepairOrderStart { get; set; }
+    [DataType( DataType.Date )]
+    public DateTime RepairOrderFinish { get; set; }
+
+
+}
---
+++ Bike_service/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bike_service.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+}
