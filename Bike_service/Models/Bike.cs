2f8f01183cbecbb87427281186a22d446841c8d6
Bartłomiej Nalepa <bnalepa@o2.pl>
2023-01-21 20:57:41 +0100

    Starting

--- Bike_service/Models/Bike.cs
+++ Bike_service/Models/Bike.cs
@@ -8,20 +8,6 @@ public class Bike
 
 {
     //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-    [Required(ErrorMessage ="Enter valid name!")]
-    [StringLength(50)]
-    public string Name { get; set; }
-    public int FrameLength { get; set; }
-    public string TyreType { get; set; }
-    public int WheelSize { get; set; }
-
-    public string BikeType { get; set;}
-
-    [DataType(DataType.Date)]
-    public DateTime RepairOrderStart { get; set; }
-    [DataType( DataType.Date )]
-    public DateTime RepairOrderFinish { get; set; }
 
 
 }
