41b4113048d3bc2513bb664882a76b3cc446ad43
Bartłomiej Nalepa <bnalepa@o2.pl>
2023-01-21 22:25:55 +0100

    Add RentalModel and Rental Index

---
+++ Bike_service/Controllers/RentalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bike_service.Models;
+
+namespace Bike_service.Controllers
+{
+    public class RentalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Bike_service/Models/RentalModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace Bike_service.Models;
+
+
+public class Rental
+
+{
+    //[HiddenInput]
+    //public int Id { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać imię!")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "Proszę podać nazwisko!")]
+    public string Surname { get; set; }
+    [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+    [Required(ErrorMessage = "Proszę podać poprawny email!")]
+    public string Email { get; set; }
+    [Phone]
+    [Required(ErrorMessage = "Proszę podać poprawny numer telefonu!")]
+    public int Phone { get; set; }
+    [Required(ErrorMessage = "Proszę podać poprawny numer telefonu!")]
+    [DataType(DataType.Date)]
+    public int StartDate { get; set; }
+    [DataType(DataType.Date)]
+    public int EndDate { get; set; }
+}
