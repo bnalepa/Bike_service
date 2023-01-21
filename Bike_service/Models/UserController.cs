52814b25a165e04c1de0d0445a57b776009b7674
Bartłomiej Nalepa <bnalepa@o2.pl>
2023-01-21 23:20:07 +0100

    Add login page

---
+++ Bike_service/Models/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace Bike_service.Models;
+
+    public class UserController
+    {
+
+    }
