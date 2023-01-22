ce49f8135e12e2df304a1778fa5bbb9706e02473
Bartłomiej Nalepa <bnalepa@o2.pl>
2023-01-22 21:52:26 +0100

    Add login option

--- Bike_service/Models/UserController.cs
+++ Bike_service/Models/UserController.cs
@@ -7,5 +7,8 @@ namespace Bike_service.Models;
 
     public class UserController
     {
-
+    [Required(ErrorMessage = "Podaj login!")]
+    public string Login {get; set; }
+    [DataType(DataType.Password)]
+    public string Password {get; set; }
     }
