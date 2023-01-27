6440e5059a40245f6f66dcaa9195030b7b90f8e6
Bartłomiej Nalepa <bnalepa@o2.pl>
2023-01-27 07:34:19 +0100

    Fix migrations

--- Bike_service/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Bike_service/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -113,7 +113,22 @@ namespace Bike_service.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var user = await _signInManager.UserManager.FindByNameAsync(Input.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, Input.Password, false);
+                if (result.Succeeded)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim("amr", "pwd"),
+                    };
+                }
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
